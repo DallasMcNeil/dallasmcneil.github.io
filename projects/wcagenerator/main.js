7e62419bd41ea95a45a678d2dde13813229396e6
Dallas McNeil <dallas.mcneil@gmail.com>
2022-10-19 23:12:16 +1030

    WCA gen templates and start of book type

--- projects/wcagenerator/main.js
+++ projects/wcagenerator/main.js
@@ -1,48 +1,50 @@
-
 var wcif = undefined
 var backgroundImage = ""
 
-var badgeFormats = [
+var badgeTemplates = [
     {
-        name: "A4 Basic 3x3",
+        name: "SCA Basic 3x3",
         description: "3 rows of 3 badges for printing on a landscape A4 page, no schedule",
+        link: "./templates/SCA-standard.html",
         pageWidth: 29.7,
         pageHeight: 20.9818,
         pageRows: 3,
         pageColumns: 3,
         badgeWidth: 9.9,
         badgeHeight: 7,
-        templates: [
-            {
-                name: "SCA Standard",
-                link: "./templates/SCA-standard.html",
-            }
-        ]
+        badgeScale: 1.0,
     },
     {
-        name: "A6 Book",
+        name: "SCA Book",
         description: "Individual badge and schedule for printing on a single landscape A6 page",
-        pageWidth: 10.5,
-        pageHeight: 14.85,
+        link: "./templates/SCA-book.html",
+        pageWidth: 29.7,
+        pageHeight: 20.9818,
         pageRows: 1,
         pageColumns: 1,
-        badgeWidth: 10.5,
-        badgeHeight: 14.85,
-        templates: [
-            {
-                name: "SCA Book",
-                link: "./templates/SCA-book.html",
-            }
-        ]
+        badgeWidth: 29.7,
+        badgeHeight: 20.9818,
+        badgeScale: 1.0,
+    },
+    {
+        name: "SCA Book 2x2",
+        description: "2 rows of 2 columns of badges and schedule for printing on a landscape A4 page",
+        link: "./templates/SCA-book.html",
+        pageWidth: 29.7,
+        pageHeight: 20.9818,
+        pageRows: 2,
+        pageColumns: 2,
+        badgeWidth: 29.7,
+        badgeHeight: 20.9818,
+        badgeScale: 0.5,
     }
 ]
 
 var settings = {
-    badgeFormat: 0,
     template: 0,
     customTemplate: false,
     marginPercentage: 0.05,
-}
+} 
 
 function readWCIF(input) {
     let file = input.files[0]; 
@@ -102,8 +104,7 @@ function generate() {
         return 0;
     });
 
-    var format = badgeFormats[settings.badgeFormat];
-    var template = format.templates[settings.template];
+    var template = badgeTemplates[settings.template];
 
     $("#template").load(template.link, function() {
 
@@ -112,14 +113,14 @@ function generate() {
         var doc = $("#print-document");
         doc.empty();
     
-        doc.css("width",`${format.pageWidth}cm`);
+        doc.css("width",`${template.pageWidth}cm`);
     
         var index = 0;
         var pageIndex = 0;
         var badgeIndex = 0;
         
         while (true) {
-            if (badgeIndex >= (format.pageRows * format.pageColumns)) {
+            if (badgeIndex >= (template.pageRows * template.pageColumns)) {
                 if (index >= persons.length) {
                     break;
                 }
@@ -128,14 +129,16 @@ function generate() {
             }
     
             if (badgeIndex == 0) {
-                var newPage = $(`<div class='print-page' style='width:${format.pageWidth}cm;height:${format.pageHeight}cm'></div>`)
+                var newPage = $(`<div class='print-page' style='width:${template.pageWidth}cm;height:${template.pageHeight}cm'></div>`)
                 newPage.css("transform",`scale(${1.0 - settings.marginPercentage})`)
                 doc.append(newPage)
             }
     
             var badge = $('#badge-template').children().first().clone();
-            badge.css("width",`${format.badgeWidth}cm`);
-            badge.css("height",`${format.badgeHeight}cm`);
+            badge.css("width",`${template.badgeWidth}cm`);
+            badge.css("height",`${template.badgeHeight}cm`);
+            badge.css("transform",`scale(${template.badgeScale})`)
+            badge.css("transform-origin",`0% 0%`)
             badge.removeAttr("id");
             badge.show();
             if (index >= persons.length) {
@@ -145,8 +148,8 @@ function generate() {
                 badge.find(".wca-name").first().text(persons[index].name);
                 badge.find(".wca-id").first().text(persons[index].wcaId);
             }
-            badge.css("top", `${(Math.floor(badgeIndex / format.pageColumns)) * format.badgeHeight}cm`)
-            badge.css("left", `${(badgeIndex % format.pageRows) * format.badgeWidth}cm`)
+            badge.css("top", `${(Math.floor(badgeIndex / template.pageColumns)) * template.badgeHeight * template.badgeScale}cm`)
+            badge.css("left", `${(badgeIndex % template.pageRows) * template.badgeWidth * template.badgeScale}cm`)
     
             var page = doc.find(".print-page").last();
             page.append(badge);
@@ -178,6 +181,18 @@ function print() {
     }, 1000);
 }
 
+// Set badge dropdown
+var option = '';
+for (var i=0;i<badgeTemplates.length;i++){
+   option += '<option value="' + i + '">' + badgeTemplates[i].name + '</option>';
+}
+$('#select-template').html(option);
+$('#select-template').val(String(settings.template));
+$('#select-template').on('change', function() {
+    settings.template = Number(this.value);
+});
+
 setTimeout(function() {
     generate();
 }, 1000);
+

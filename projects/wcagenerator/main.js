db17f346f00ad1bbc8980d62ddc91782ed3febf2
Dallas McNeil <dallas.mcneil@gmail.com>
2022-10-21 11:19:06 +1030

    Template selection, book type and schedules

--- projects/wcagenerator/main.js
+++ projects/wcagenerator/main.js
@@ -1,5 +1,38 @@
+
 var wcif = undefined
+var activities = {}
 var backgroundImage = ""
+
+var weekDaysText = [
+    "Sunday", 
+    "Monday", 
+    "Tuesday", 
+    "Wednesday", 
+    "Thursday", 
+    "Friday", 
+    "Saturday", 
+]
+
+var eventText = {
+    "222": "2x2x2",
+    "333": "3x3x3",
+    "333fm": "Fewest Moves",
+    "333ft": "3x3x3 With Feet",
+    "333oh": "3x3x3 One-Handed",
+    "333mbf": "3x3x3 Multi-Blind",
+    "333bf": "3x3x3 Blindfolded",
+    "444bf": "4x4x4 Blindfolded",
+    "555bf": "5x5x5 Blindfolded",
+    "444": "4x4x4",
+    "555": "5x5x5",
+    "666": "6x6x6",
+    "777": "7x7x7",
+    "sq1": "Square-1",
+    "clock": "Clock",
+    "minx": "Megaminx",
+    "pyram": "Pyraminx",
+    "skewb": "Skewb",
+}
 
 var badgeTemplates = [
     {
@@ -46,16 +79,51 @@ var settings = {
     marginPercentage: 0.05,
 } 
 
+function fitText(nameElem, lines) {
+    var height = nameElem.height();
+    var maxHeight = parseInt(nameElem.css("font-size"), 10) * lines;
+    while (maxHeight < height) {
+        size = parseInt(nameElem.css("font-size"), 10);
+        nameElem.css("font-size", size - 1);
+        height = nameElem.height();
+
+        var bottom = parseInt(nameElem.css("bottom"), 10);
+        nameElem.css("bottom", bottom + 1);
+    }
+}
+
 function readWCIF(input) {
     let file = input.files[0]; 
     let fileReader = new FileReader(); 
     fileReader.readAsText(file); 
     fileReader.onload = function() {
         wcif = JSON.parse(fileReader.result);
-        if (wcif == undefined)
-        {
+        if (wcif == undefined) {
             alert("Invalid WCIF file provided");
         }
+
+        activities = {}
+        for (var v=0; v<wcif.schedule.venues.length; v++) {
+            var venue = wcif.schedule.venues[v];
+            for (var r=0; r<venue.rooms.length; r++) {
+                var room = venue.rooms[r];
+                for (var a=0; a<room.activities.length; a++) {
+                    var activity = room.activities[a];
+                    for (var c=0; c<activity.childActivities.length; c++) {
+                        var childActivity = activity.childActivities[c];
+
+                        activities[childActivity.id] = {
+                            parentActivityCode: activity.activityCode,
+                            activityCode: childActivity.activityCode,
+                            roundStartTime: activity.startTime,
+                            roundEndTime: activity.endTime,
+                            timezone: venue.timezone,
+                        }
+                    }
+                }
+            }
+        }
+        console.log(activities)
     }; 
     fileReader.onerror = function() {
         alert(fileReader.error);
@@ -150,13 +218,127 @@ function generate() {
             }
             badge.css("top", `${(Math.floor(badgeIndex / template.pageColumns)) * template.badgeHeight * template.badgeScale}cm`)
             badge.css("left", `${(badgeIndex % template.pageRows) * template.badgeWidth * template.badgeScale}cm`)
-    
+
             var page = doc.find(".print-page").last();
             page.append(badge);
+
+            fitText(badge.find(".wca-name").first(), 2);
     
+            var personalSchedule = {}
+            if (index >= persons.length) {
+            } else {
+                for (var a=0; a<persons[index].assignments.length; a++) {
+                    var assignment = persons[index].assignments[a];
+                    var activity = activities[assignment.activityId];
+                    if (activities[assignment.activityId] == undefined) {
+                        console.warn(`MISSING ACTIVITY ${activityId}`);
+                    } else {
+                        var startTime = moment(activity.roundStartTime).tz(activity.timezone); 
+                        var endTime = moment(activity.roundEndTime).tz(activity.timezone);
+
+                        var day = startTime.day();
+                        if (personalSchedule[day] == undefined) {
+                            personalSchedule[day] = {
+                                day: day,
+                                sortTime: startTime.unix(),
+                                assignments: {},
+                                sortedAssignments: [],
+                            }
+                        }
+
+                        //console.log(`${assignment.assignmentCode} in ${activity.activityCode}`)
+                        var codes = activity.activityCode.split('-')
+                        var event = codes[0]
+                        var group = codes[2]
+
+                        if (personalSchedule[day].assignments[activity.parentActivityCode] == undefined) {
+                            personalSchedule[day].assignments[activity.parentActivityCode] = {
+                                timeText: `${startTime.format("HH:mm")} - ${endTime.format("HH:mm")}`,
+                                sortTime: startTime.unix(),
+                                eventCode: event,
+                                eventText: eventText[event],
+                                competing: -1,
+                                judging: [],
+                            };
+                        }
+                        
+                        if (assignment.assignmentCode == "competitor") {
+                            personalSchedule[day].assignments[activity.parentActivityCode].competing = group.substr(1);
+                        } else if (assignment.assignmentCode == "staff-judge") {
+                            personalSchedule[day].assignments[activity.parentActivityCode].judging.push(group.substr(1));
+                        } else {
+                            console.warn(`MISSING ASSIGNMENT CODE ${assignment.assignmentCode}`);
+                        }
+                        
+                    }
+                }
+
+                var sortedSchedule = []
+                for (let value of Object.values(personalSchedule)) {
+                    sortedSchedule.push(value);
+                }
+
+                sortedSchedule.sort((a,b) => {
+                    if (a.sortTime < b.sortTime) {
+                        return -1;
+                    }
+                    if (a.sortTime > b.sortTime) {
+                        return 1;
+                    }
+                    return 0;
+                });
+
+                for (var i=0; i<sortedSchedule.length; i++) {   
+                    for (let value of Object.values(sortedSchedule[i].assignments)) {
+                        sortedSchedule[i].sortedAssignments.push(value);
+                    }
+                    
+                    sortedSchedule[i].sortedAssignments.sort((a,b) => {
+                        if (a.sortTime < b.sortTime) {
+                            return -1;
+                        }
+                        if (a.sortTime > b.sortTime) {
+                            return 1;
+                        }
+                        return a.eventCode > b.eventCode;
+                    });
+                }
+
+                //console.log(personalSchedule)
+                //console.log(sortedSchedule)
+
+                var table = badge.find(".wca-schedule").first();
+                var tableContent = "";
+                tableContent += "<tr><td>Time</td><td>Event</td><td>Group</td><td>Roles</td></tr>";
+                for (var i=0; i<sortedSchedule.length; i++) {   
+                    tableContent += `<tr><td colspan="4" class="wca-schedule-header">${ weekDaysText[sortedSchedule[i].day]}</td></tr>`
+                    for (var j=0; j<sortedSchedule[i].sortedAssignments.length; j++) {   
+                        var assignment = sortedSchedule[i].sortedAssignments[j];
+
+                        var roleText = "";
+                        for (var k=0; k<assignment.judging.length; k++) {
+                            if (k == 0) {
+                                roleText += "Judging:"
+                            }
+                            if (k == 0) {
+                                roleText += ` ${assignment.judging[k]}`
+                            } else {
+                                roleText += `, ${assignment.judging[k]}`
+                            }
+                        }
+
+                        var eventIcon = `<i class="cubing-icon icon event-${assignment.eventCode}"></i>`
+
+                        tableContent += `<tr><td>${assignment.timeText}</td><td>${eventIcon} ${assignment.eventText}</td><td>${assignment.competing}</td><td>${roleText}</td></tr>`;
+                    }
+                }
+                table.append(tableContent);
+            }
+
             badgeIndex+=1;
             index+=1;
         }
+
     });
 }
 
@@ -165,6 +347,7 @@ function print() {
 
     printwin.document.open();
     printwin.document.write('<html><head><link rel="stylesheet" type="text/css" href="./style.css"></head><body>');
+    printwin.document.write('<html><head><link rel="stylesheet" type="text/css" href="./cubingIcons.css"></head><body>');
     printwin.document.write('<html><head><link rel="stylesheet" type="text/css" href="./print.css"></head><body>');
     printwin.document.write($("#badge-template-style").prop('outerHTML'));
     if ($("#background-image-style").prop('outerHTML') != undefined) {
@@ -193,6 +376,6 @@ $('#select-template').on('change', function() {
 });
 
 setTimeout(function() {
-    generate();
+    
 }, 1000);
 
